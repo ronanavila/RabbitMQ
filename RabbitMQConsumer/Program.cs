0b7ca12fcf4ff669fedd54d6b81cf0c871e5d01a
Ronan Avila <avila.ronan@gmail.com>
2022-07-28 11:24:23 -0300

    FEAT: pusblisher consumer RabbitMQ

---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+};
+
+using (var connection = factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare(queue: "mainQueue", durable: false,
+            exclusive: false, autoDelete: false, arguments: null);
+
+        var consumerEventing = new EventingBasicConsumer(channel);
+
+        consumerEventing.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            Console.WriteLine($"Message Received: {message}");
+        };
+
+        channel.BasicConsume(queue: "mainQueue", autoAck: true, consumer: consumerEventing);
+
+        Console.ReadLine();
+        
+    }
+}
---
+++ RabbitMQPublisher/Program.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+};
+
+using (var connection = factory.CreateConnection())
+{
+    using(var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare(queue: "mainQueue", durable: false, 
+            exclusive: false, autoDelete: false, arguments: null);
+
+        string message = "Queue message";
+
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(exchange: "", routingKey: "mainQueue",
+            basicProperties: null, body: body);
+
+        Console.WriteLine($"Message Sent: {message}");
+    }
+}
